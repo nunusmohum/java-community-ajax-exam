b597449404cb81408003f03bf8e137e0ec434b7d
nunusmohum <halojw301@gmail.com>
2022-07-28 11:26:49 +0900

    objToStr, jsonToObj 추가

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,4 +1,3 @@
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.ll.exam.article.dto.ArticleDto;
 import com.ll.exam.util.Ut;
 import org.junit.jupiter.api.Test;
@@ -7,13 +6,29 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class AppTest {
     @Test
-    void 실험_ObjectMapper() throws JsonProcessingException {
+    void assertJ__assertThat() {
+        int rs = 10 + 20;
+        assertThat(rs).isEqualTo(30);
+    }
+
+    @Test
+    void ObjectMapper__objToJsonStr() {
         ArticleDto articleDto = new ArticleDto(1, "제목", "내용");
 
-        String jsonStr = Ut.json.toJsonStr(articleDto, "");
+        String jsonStr = Ut.json.toStr(articleDto, "");
         assertThat(jsonStr).isNotBlank();
         assertThat(jsonStr).isEqualTo("""
                 {"id":1,"title":"제목","body":"내용"}
                 """.trim());
+    }
+
+    @Test
+    void ObjectMapper__jsonStrToObj() {
+        ArticleDto articleDtoOrigin = new ArticleDto(1, "제목", "내용");
+        String jsonStr = Ut.json.toStr(articleDtoOrigin, "");
+
+        ArticleDto articleDtoFromJson = Ut.json.toObj(jsonStr, ArticleDto.class, null);
+
+        assertThat(articleDtoOrigin).isEqualTo(articleDtoFromJson);
     }
 }
