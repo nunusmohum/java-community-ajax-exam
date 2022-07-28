0498efdd0d14c08b9866aaa88f164991e0ff654e
nunusmohum <halojw301@gmail.com>
2022-07-28 10:18:52 +0900

    test, json 컨버팅 라이브러리 설치

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,19 @@
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.ll.exam.article.dto.ArticleDto;
+import com.ll.exam.util.Ut;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AppTest {
+    @Test
+    void 실험_ObjectMapper() throws JsonProcessingException {
+        ArticleDto articleDto = new ArticleDto(1, "제목", "내용");
+
+        String jsonStr = Ut.json.toJsonStr(articleDto, "");
+        assertThat(jsonStr).isNotBlank();
+        assertThat(jsonStr).isEqualTo("""
+                {"id":1,"title":"제목","body":"내용"}
+                """.trim());
+    }
+}
